8d6e5f4345a495d086a5311453652f93df9ffb59
Gabriel Barbosa <gabriel.brbs84@gmail.com>
2023-12-16 11:34:36 -0300

    Desafio Estacionamento Concluído + Resolução de problema na versão do dotnet

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -4,17 +4,17 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoHora = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+precoHora = Convert.ToDecimal(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+Estacionamento es = new Estacionamento(precoInicial, precoHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
